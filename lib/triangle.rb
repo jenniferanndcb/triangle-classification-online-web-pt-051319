7cbce1fd764be7c6de7fb9f25444d6e50061b368
Jen <jenniferanndcb@gmail.com>
2019-06-19 10:04:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,14 +10,15 @@ class Triangle
   
   def kind 
     case 
+      when (a + b <= c) || (b + c <= a) || (a + c <= b) || a == 0 && b == 0 && c == 0 
+        raise TriangleError
       when a == b && a == c 
         :equilateral
       when a == b || a == c ||b == c
         :isosceles 
       when a != b && a != c && b != c 
         :scalene 
-      when (a + b <= c) || (b + c <= a) || (a + c <= b) || a == 0 && b == 0 && c == 0 
-        raise TriangleError
+      
     end 
   
   end 
